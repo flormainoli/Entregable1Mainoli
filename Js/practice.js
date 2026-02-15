e8b53e286a134ccbd23b1eabe961be31a627f255
flormainoli <florenciamainoli@gmail.com>
2026-02-15 13:36:23 -0300

    commit entrega 2

--- Js/practice.js
+++ Js/practice.js
@@ -1,220 +1,157 @@
-const cafes = [
-  {
-    nombre: "Cuervo Café",
-    precio: "alto",
-    cafe: "espresso",
-    trabajar: false,
-    platoDestacado: "salado",
-    petfriendly: true,
-    ambiente: "concurrido",
-    estrellas: false,
-    estilo: "moderno",
-  },
-  {
-    nombre: "Usina Cafetera Belgrano",
-    precio: "alto",
-    cafe: "latte",
-    trabajar: true,
-    platoDestacado: "pasteleria",
-    petfriendly: true,
-    ambiente: "tranquilo",
-    estrellas: false,
-    estilo: "tradicional",
-  },
-  {
-    nombre: "Gorrion Congreso",
-    precio: "medio",
-    cafe: "iced",
-    trabajar: true,
-    platoDestacado: "brunch",
-    petfriendly: true,
-    ambiente: "tranquilo",
-    estrellas: true,
-    estilo: "moderno",
-  },
-  {
-    nombre: "Pocho Cafe",
-    precio: "medio",
-    cafe: "latte",
-    trabajar:true,
-    platoDestacado:"brunch",
-    petfriendly:true,
-    ambiente:"tranquilo",
-    estrellas:true,
-    estilo:"moderno"
-  },
-    {
-    nombre: "Pulso Speciality Coffe",
-    precio: "medio",
-    cafe: "espresso",
-    trabajar:true,
-    platoDestacado:"Salado",
-    petfriendly:true,
-    ambiente:"tranquilo",
-    estrellas:true,
-    estilo:"tradicional",
-  },
-    {
-    nombre: "Denkins",
-    precio: "alto",
-    cafe: "latte",
-    trabajar:false,
-    platoDestacado:"Pasteleria",
-    petfriendly:true,
-    ambiente:"concurrido",
-    estrellas:true,
-    estilo:"moderno",
-  },
-  {
-    nombre: "Maru Botana",
-    precio: "medio",
-    cafe: "latte",
-    trabajar:false,
-    platoDestacado:"Pasteleria",
-    petfriendly:true,
-    ambiente:"concurrido",
-    estrellas:false,
-    estilo:"tradicional",
-  },
-  {
-    nombre: "Stumpy coffe point",
-    precio: "bajo",
-    cafe: "latte",
-    trabajar:true,
-    platoDestacado:"brunch",
-    petfriendly:true,
-    ambiente:"tranquilo",
-    estrellas:false,
-    estilo:"tradicional",
-  },
-    {
-    nombre: "Caza Nueza",
-    precio: "alto",
-    cafe: "iced",
-    trabajar:true,
-    platoDestacado:"Pasteleria",
-    petfriendly:true,
-    ambiente:"tranquilo",
-    estrellas:false,
-    estilo:"moderno",
-  },
-  {
-    nombre: "Co-Pain",
-    precio: "bajo",
-    cafe: "espresso",
-    trabajar:true,
-    platoDestacado:"pasteleria",
-    petfriendly:true,
-    ambiente:"tranquilo",
-    estrellas:true,
-    estilo:"tradicional",
+//Arrays de los Cafes
+class Cafe {
+  static id = 0;
+  constructor( nombre,precio,cafe,trabajar,platoDestacado,petfriendly,ambiente,estrellas,estilo) {
+    this.id = Cafe.id++;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cafe = cafe;
+    this.trabajar = trabajar;
+    this.platoDestacado = platoDestacado;
+    this.petfriendly = petfriendly;
+    this.ambiente = ambiente;
+    this.estrellas = estrellas;
+    this.estilo = estilo;
   }
-];
-function pedirOpcionValida(mensaje, opcionesValidas) {
-  let respuesta = prompt(mensaje).toLowerCase();
-
-  while (!opcionesValidas.includes(respuesta)) {
-    alert(
-      "Opción incorrecta.\n" +
-      "Ingresá una de las siguientes opciones:\n" +
-      opcionesValidas.join(" / ")
-    );
-    respuesta = prompt(mensaje).toLowerCase();
-  }
-
-  return respuesta;
 }
+const cafeQuiz = [];
+cafeQuiz.push(
+  new Cafe("cuervo café", "alto","espresso",false,"salado",true, "concurrido", false,"moderno")
+);
 
-//Entrada de Datos
-function obtenerRespuestasUsuario() {
-  let respuestas = {};
+cafeQuiz.push(
+  new Cafe("úsina cafetera","alto","latte",true,"pasteleria",true,"tranquilo", false, "tradicional"
+  )
+);
+cafeQuiz.push(
+  new Cafe("gorrión café","medio","latte",true,"brunch",true,"tranquilo",false,"moderno")
+);
+cafeQuiz.push(
+  new Cafe("pocho café","medio","latte",true,"brunch",true,"tranquilo",false,"moderno"
+  )
+);
+cafeQuiz.push(
+  new Cafe("pulso speciality coffe","medio","espresso",true,"salado",true,"tranquilo",false,"tradicional")
+);
+  cafeQuiz.push(
+  new Cafe("deniks","alto","latte",false,"pasteleria",true,"concurrido",true,"moderno")
+);
+cafeQuiz.push(
+  new Cafe("maru botana café","medio","latte",false,"pasteleria",true,"concurrido",false,"tradicional")
+);
+  cafeQuiz.push(
+  new Cafe("stumpy coffee point","bajo","latte", true,"brunch",true,"tranquilo", false,"tradicional")
+);
+  cafeQuiz.push(
+  new Cafe("co-pain","bajo","espresso",true,"pasteleria",true,"tranquilo",false,"tradicional")
+);
+//Calcular el mejor café según las respuestas del usuario
 
-  respuestas.ambiente = pedirOpcionValida(
-    "¿Qué ambiente preferís?\n- tranquilo\n- concurrido",
-    ["tranquilo", "concurrido"]
-  );
+function calcularMejorCafe(cafeQuiz, respuestasUsuario) {
+  const resultados = [];
 
-  respuestas.precio = pedirOpcionValida(
-    "¿Qué rango de precio buscás?\n- bajo\n- medio\n- alto",
-    ["bajo", "medio", "alto"]
-  );
-
-  respuestas.petfriendly = (
-    "¿Querés que la cafetería sea pet friendly?"
-  );
-
-  respuestas.platoDestacado = pedirOpcionValida(
-    "¿Qué te gusta acompañar con el café?\n- pasteleria\n- brunch\n- salado",
-    ["pasteleria", "brunch", "salado"]
-  );
-
-  respuestas.estilo = pedirOpcionValida(
-    "¿Qué estilo preferís?\n- moderno\n- tradicional",
-    ["moderno", "tradicional"]
-  );
-  respuestas.cafe = pedirOpcionValida(
-    "¿Qué tipo de café preferís?\n- espresso\n- latte\n- iced",
-    ["espresso", "latte", "iced"]
-  );
-  respuestas.trabajar = confirm(
-    "¿Necesitás un lugar para trabajar?"
-  );
-  respuestas.estrellas = confirm(
-    "¿Te importa que la cafetería tenga buenas reseñas?"
-  );
-
-  return respuestas;
-}
-
-
-
-//Procesamiento
-
-function calcularMejorCafe(cafes, respuestasUsuario) {
-  let resultados = [];
-
-  for (let i = 0; i < cafes.length; i++) {
+  for (const cafe of cafeQuiz) {
     let puntaje = 0;
 
-    if (cafes[i].ambiente === respuestasUsuario.ambiente) puntaje++;
-    if (cafes[i].precio === respuestasUsuario.precio) puntaje++;
-    if (cafes[i].petfriendly === respuestasUsuario.petfriendly) puntaje++;
-    if (cafes[i].estilo === respuestasUsuario.estilo) puntaje++;
-
-    if (cafes[i].platoDestacado === respuestasUsuario.platoDestacado) {
-      puntaje += 2;
-    }
+    if (cafe.ambiente === respuestasUsuario.ambiente) puntaje++;
+    if (cafe.precio === respuestasUsuario.precio) puntaje++;
+    if (cafe.petfriendly === respuestasUsuario.petfriendly) puntaje++;
+    if (cafe.estilo === respuestasUsuario.estilo) puntaje++;
+    if (cafe.cafe === respuestasUsuario.cafe) puntaje++;
+    if (cafe.estrellas === respuestasUsuario.estrellas) puntaje++;
+    if (cafe.opcionesComida === respuestasUsuario.opcionesComida) puntaje++;
+    if (cafe.platoDestacado === respuestasUsuario.platoDestacado) {puntaje += 2;}
 
     resultados.push({
-      nombre: cafes[i].nombre,
+      nombre: cafe.nombre,
       puntaje: puntaje
     });
   }
-  
-
   let mejorCafe = resultados[0];
-
-  for (let i = 1; i < resultados.length; i++) {
-    if (resultados[i].puntaje > mejorCafe.puntaje) {
-      mejorCafe = resultados[i];
+  for (const resultado of resultados) {
+    if (resultado.puntaje > mejorCafe.puntaje) {
+      mejorCafe = resultado;
     }
   }
 
   return mejorCafe;
 }
+//Obtener respuestas del Usuario
+const preguntas = document.querySelectorAll(".question");
+const botonesOpciones = document.querySelectorAll(".option-btn");
 
-//Salida de Datos
-function mostrarResultado(mejorCafe) {
-  alert(
-    " RESULTADO DEL QUIZ:  " +
-    "Tu cafetería ideal es  " +
-    mejorCafe.nombre  );
+let preguntaActual = 0;
+let respuestasUsuario = {};
+botonesOpciones.forEach(boton => {
+  boton.addEventListener("click", () => {
+    const eleccionusuario = boton.dataset.value;
+    const numeroPregunta = boton.closest(".question").dataset.question;
+    guardarRespuesta(numeroPregunta, eleccionusuario);
 
-  console.log("La cafetería recomendada es:", mejorCafe.nombre);
+  });
+});
+
+function guardarRespuesta(numeroPregunta, valor) {
+
+  switch (numeroPregunta) {
+    case "1":respuestasUsuario.ambiente = valor;
+      break;
+    case "2":respuestasUsuario.cafe = valor;
+      break;
+    case "3":respuestasUsuario.petfriendly = valor === "true";
+      break;
+    case "4":respuestasUsuario.precio = valor;
+      break;
+    case "5":respuestasUsuario.platoDestacado = valor;
+      break;
+    case "6":respuestasUsuario.estilo = valor;
+      break;
+    case "7":respuestasUsuario.estrellas = valor === "true";
+      break;
+    case "8":respuestasUsuario.opcionesComida = valor === "true";
+      break;
+  }
+
+//Mostras el resultado
+}
+function mostrarResultadoFinal() {
+  const mejorCafe = calcularMejorCafe(cafeQuiz, respuestasUsuario);
+  sessionStorage.setItem("cafeIdeal", JSON.stringify(mejorCafe));
+  mostrarCafeGuardado(mejorCafe);
 }
 
-alert("Bienvenido al QUIZ: Tu Cafetería Ideal en Belgrano. Elegi entre las opciones que prefieras, y te recomendaremos la mejor cafetería para vos.");
+function mostrarCafeGuardado(mejorCafe) {
+  const resultadoDiv =document.getElementById("resultado");
+  const botonResultado =document.getElementById("verResultado");
+  if (botonResultado) {botonResultado.style.display = "none";
+  } 
+  resultadoDiv.innerHTML = `
+    <div class="card resultadoDiv p-4 shadow text-center">
+      <h2>Tu cafetería ideal es:</h2>
+      <h3 class="mt-3">
+        ${mejorCafe.nombre}
+      </h3>
 
-let respuestasUsuario = obtenerRespuestasUsuario();
-let mejorCafe = calcularMejorCafe(cafes, respuestasUsuario);
-mostrarResultado(mejorCafe);
+      <button id="rehacerQuiz" class="btn btn-outline-dark mt-3">
+        Rehacer quiz
+      </button>
+
+    </div>
+  `;
+
+  document.getElementById("rehacerQuiz").addEventListener("click", function () {
+             sessionStorage.removeItem("cafeIdeal");
+             location.reload()
+    });
+}
+
+document.getElementById("verResultado") .addEventListener("click",mostrarResultadoFinal);
+document.addEventListener("DOMContentLoaded",
+  function () {
+    const cafeGuardado =sessionStorage.getItem("cafeIdeal");
+    if (cafeGuardado) {
+      const cafe =JSON.parse(cafeGuardado);
+      mostrarCafeGuardado(cafe);
+    }
+  });
+
